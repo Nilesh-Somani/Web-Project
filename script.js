f20c8ab767756bff5a3b6c0759584c10cbe03b90
Nilesh Somani <somanin408@gmail.com>
2023-10-21 22:48:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
-      toggle = body.querySelector(".toggle"),
+      toggle = sidebar.querySelector(".toggle"),
       searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
@@ -10,10 +10,4 @@ const body = document.querySelector("body"),
       });
       modeSwitch.addEventListener("click", () => {
         body.classList.toggle("dark");
-
-        if(body.classList.contains("dark")) {
-            modeText.innerText = "Light Mode";
-        } else {
-            modeText.innerText = "Dark Mode";
-        }
       });
